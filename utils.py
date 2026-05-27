a452ffcf45aefcda484c9469836add822c06c153
Ania Szkaradek <ania.szkaradek@interia.pl>
2026-05-27 16:25:28 +0200

    Add CI/CD workflow, utils and tests

---
+++ utils.py
@@ -0,0 +1,21 @@
+"""Moduł z podstawowymi operacjami matematycznymi."""
+
+
+def add(a: int, b: int) -> int:
+    """Dodaje dwie liczby."""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Odejmuje dwie liczby."""
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """Mnoży dwie liczby."""
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """Dzieli dwie liczby."""
+    return a / b
