4cdf951efefc728a3314a4538e43c0e4847c20de
aniaszkaradek <ania.szkaradek@interia.pl>
2026-05-27 18:06:45 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,21 @@
-# przykladowe funkcje utils .py
-def add ( a: int , b: int) -> int:
+"""Moduł z podstawowymi operacjami matematycznymi."""
+
+
+def add(a: int, b: int) -> int:
+    """Dodaje dwie liczby."""
     return a + b
-    
-def subtract ( a: int , b: int) -> int:
+
+
+def subtract(a: int, b: int) -> int:
+    """Odejmuje dwie liczby."""
     return a - b
-    
-def multiply ( a: int , b: int) -> int:
+
+
+def multiply(a: int, b: int) -> int:
+    """Mnoży dwie liczby."""
     return a * b
-    
-def divide ( a: int , b: int) -> float :
+
+
+def divide(a: int, b: int) -> float:
+    """Dzieli dwie liczby."""
     return a / b
